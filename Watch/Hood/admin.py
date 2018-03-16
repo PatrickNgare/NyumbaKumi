cabb7be51d3f35d2f5e9fea2f691f1f155d3e284
patrickNgare <patrickngare@gmail.com>
2018-03-16 16:18:59 +0300

    register the models in admin py

--- Watch/Hood/admin.py
+++ Watch/Hood/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from .models import Business,User_profile,Neigbourhood
 
 # Register your models here.
 
-   
+admin.site.register(Business)
+admin.site.register(User_profile)
+admin.site.register(Neigbourhood)
