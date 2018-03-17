337a33abeba2feb74491185a48bec25d3f6e8249
patrickNgare <patrickngare@gmail.com>
2018-03-17 13:13:15 +0300

    added edit profile link function

--- Watch/Hood/views.py
+++ Watch/Hood/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from .forms import EditProfile
 
 @login_required(login_url='/accounts/login/')
 def index(request):
@@ -13,3 +15,20 @@ def profile(request):
     
     
     return render(request,'temps/profile.html')
+
+
+
+
+@login_required(login_url='/accounts/login/')
+@transaction.atomic
+def editprofile(request):
+    if request.method == 'POST':
+        profile_form = EditProfile(request.POST, request.FILES, instance=request.user)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('index')
+    else:
+        
+        profile_form = EditProfile(instance=request.user)
+    return render(request, 'temps/editprofile.html', {"profile_form": profile_form})    
