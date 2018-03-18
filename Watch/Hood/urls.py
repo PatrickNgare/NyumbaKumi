a4293a2f8cc8ff534d6b431ed2eaebe9216795fb
patrickNgare <patrickngare@gmail.com>
2018-03-18 14:32:51 +0300

    added urls

--- Watch/Hood/urls.py
+++ Watch/Hood/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
-    url(r'^profile',views.profile,name='profile'),
-    url(r'^editprofile', views.editprofile, name='editprofile'),
-  ]
+    
+url(r'^$',views.index,name='index'), 
+
+url(r'^upload/',views.upload, name='upload'),
+url(r'^posts/',views.post,name='post')
+
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
