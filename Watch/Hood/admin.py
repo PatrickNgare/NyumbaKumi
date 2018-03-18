adb2e8adbe503f09c51cba92b1a4c7c1e8f6ea7e
patrickNgare <patrickngare@gmail.com>
2018-03-18 14:32:04 +0300

    registered post model in the admin py

--- Watch/Hood/admin.py
+++ Watch/Hood/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Business,User_profile,Neigbourhood
+from .models import Business,User_profile,Neigbourhood,Post
 
 # Register your models here.
 
 admin.site.register(Business)
 admin.site.register(User_profile)
 admin.site.register(Neigbourhood)
+admin.site.register(Post)
