aa243088bd3886f0ba414cd1464f73a50999cf60
patrickNgare <patrickngare@gmail.com>
2018-03-19 17:21:08 +0300

    added urls fir search and singl profile

--- Watch/Hood/urls.py
+++ Watch/Hood/urls.py
@@ -12,8 +12,9 @@ url(r'^$',views.index,name='index'),
 url(r'^upload/',views.upload, name='upload'),
 url(r'^posts/',views.post,name='post'),
 url(r'^group/',views.group,name='group'),
-url(r'^allbiz/',views.allbiz,name='allbiz')
-
+url(r'^allbiz/',views.allbiz,name='allbiz'),
+url(r'^search/', views.search_results, name='search_results'),
+url(r'^single_profile/',views.single_profile, name='single_profile'),
 ]
 
 if settings.DEBUG:
